d96d446ffeb15a96b56bca0429d70cbf4a794010
wires <jelle@defekt.nl>
2017-10-09 23:54:03 +0200

    added xm player source

---
+++ xmplayer/jsxm/xmlist.js
@@ -0,0 +1,17 @@
+baseuri = "/";
+xmuris = [
+  ["alla", "unreeeal_superhero_3.xm"]
+  /*
+  ["Zalza - my dirty old kamel", "kamel.xm"],
+  ["Zalza vs. JosSs - Interc00ler", "interc00ler.xm"],
+  ["Lizardking - Claustrophobia", "Claustrophobia.xm"],
+  ["Lizardking - Transatlantic", "transatlantic.xm"],
+  ["Lizardking - Bosvedjan Jam", "bosvedjan.xm"],
+  ["xyce - ne pas", "nepas.xm"],
+  ["Jugi - Onward", "onward.xm"],
+  ["Norfair - Chillzone", "nf-chill.xm"],
+  ["Dubmood - LUCiD Keygen chip", 'db_keygen.xm'],
+  ["Strobe / TiTaN - Mothership FTW", 'mothershipftw-stroberev2.xm'],
+  ["Ghidorah - Toilet story 5", 'Ghidorah_-_Toilet_story_5.xm']*/
+];
+
